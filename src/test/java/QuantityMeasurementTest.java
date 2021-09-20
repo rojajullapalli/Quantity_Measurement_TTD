ff217e612190dc2eb3124cdd907074458f969e77
rojajullapalli <rojajules@gmail.com>
2021-09-20 10:45:43 +0530

    ADDED: Performed Test For Reference Check

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -13,14 +13,30 @@ public class QuantityMeasurementTest {
     void given0Feetand1Feet_ShouldReurnNotEqual() {
         Feet feet = new Feet(0.0);
         Feet feet1 = new Feet(1.0);
-        Assertions.assertNotEquals(feet,feet1);
+        Assertions.assertNotEquals(feet, feet1);
     }
 
     @Test
     void givenNullFeet_WhenCompared_ShouldReturnFalse() {
         Feet feet1 = null;
         Feet feet2 = new Feet(0.0);
-        Assertions.assertNotEquals(feet1,feet2);
+        Assertions.assertNotEquals(feet1, feet2);
     }
+
+    @Test
+    void givenFeetWithSameReference_WhenCompared_ShouldReturnTrue(){
+        Feet feet = new Feet(0.0);
+        boolean sameReference = feet == feet;
+        Assertions.assertTrue(sameReference);
+    }
+
+    @Test
+    void givenFeetWithDiffrentReference_WhenCompared_ShouldReturnFalse() {
+        Feet feet = new Feet(0.0);
+        Feet feet1 = new Feet(0.0);
+        boolean differentReference = feet == feet1;
+        Assertions.assertFalse(differentReference);
+    }
+
 
 }
